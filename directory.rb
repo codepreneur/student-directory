fd4798dfe68ec4476a128c4ce92b3db549b1d9bd
gomenasaai <contramundee@gmail.com>
2014-05-14 00:11:33 +0100

    final working version

--- directory.rb
+++ directory.rb
@@ -1,24 +1,6 @@
-students = [
-	 {:name => "Roi Driscoll", :cohort => :november},
-	 {:name => "James Whyte", :cohort => :november},
-	 {:name => "Margherita Serino", :cohort => :november},
-	 {:name => "Vaidas Mykolaitis", :cohort => :november},
-	 {:name => "Johann Bornman", :cohort => :november},
-	 {:name => "Kate Hamilton", :cohort => :november},
-	 {:name => "James Keap", :cohort => :november},
-	 {:name => "Nic Yeeles", :cohort => :november},
-	 {:name => "Julie Walker", :cohort => :november},
-	 {:name => "Will Allen", :cohort => :november},
-	 {:name => "Julia Tan", :cohort => :november},
-	 {:name => "Federico Maffei", :cohort => :november},
-	 {:name => "Jamie Patel", :cohort => :november},
-	 {:name => "Faezrah Rizalman", :cohort => :november},
-	 {:name => "Josh Fail-Broon", :cohort => :november},
-	 {:name => "Sasha Cooper", :cohort => :november},
-	 {:name => "Nicolai DTH", :cohort => :november},
-	 {:name => "Nadav Matalon", :cohort => :november},
-	 {:name => "Fitsum Teklehaimanot", :cohort => :november}
-	 ]
+@students = []
+
+
 
 def print_header
 	puts "The students of my cohort at Makers Academy"
@@ -27,8 +9,8 @@ end
 
 
 # first we print the list of students
-def print(students)
-	students.each do |student|
+def print_students_list
+	@students.each do |student|
 		puts "#{student[:name]} (#{student[:cohort]} cohort)"
 	end
 end
@@ -41,24 +23,69 @@ def input_students
 	puts "Please enter the names of the students"
 	puts "To finish, just hit return twice"
 	#create an empty array
-	students = []
+	# students = []
 
 	name = gets.chomp
 	while !name.empty? do
-		students << {:name => name, :cohort => :november}
-		puts "Now we have #{students.length} students"
+		@students << {:name => name, :cohort => :november}
+		puts "Now we have #{@students.length} students"
 
 		name = gets.chomp
 
 	end
 
-	students
+	# students
+end
+
+
+def show_students
+	print_header
+	print_students_list
+	print_footer(@students)
+end
+
+def process(selection)
+	case selection
+	when "1"
+		input_students
+	when "2"
+		show_students
+	when "3"
+		save_students
+	when "9"
+		exit
+	else
+		puts "I dont know what you mean, try again"
+	end
+end
+
+def print_menu
+	puts "1. for adding"
+	puts "2. for listing what you added"
+	puts "3. for saving to a csv"
+	puts "9. for bye bye"
+end
+
+def interactive_menu
+	loop do
+		print_menu
+		process(gets.chomp)
+	end
+end
+
+
+def save_students
+	file = File.open("students.csv", "w")
+	@students.each do |student|
+		student_data = [student[:name], student[:cohort]]
+		csv_line = student_data.join(",")
+		file.puts csv_line
+	end
+	file.close
 end
 
 
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
 
+interactive_menu
+save_students
