d259c1225fea3fbc121b91fc8ade45da85d03629
gomenasaai <contramundee@gmail.com>
2014-05-23 14:38:45 +0100

    added functionality: loading data

--- lib/directory.rb
+++ lib/directory.rb
@@ -1,5 +1,36 @@
 class StudentDirectory
 
+	require 'csv'
+
+	def initialize(writer, reader)
+		@writer = writer
+		@reader = reader
+		@exit = false
+	end
+
+	def students
+		@students ||= []
+	end
+
+	def load_students_from(file)
+		CSV.foreach(file) do |row|
+			import import_from(row)
+		end
+	end
+
+
+	def import(student)
+		students << student unless students.include? student
+	end
+
+
+	def import_from(row)
+		{
+			name: row[0],
+			cohort: row[1],
+			year: row[2]
+		}
+	end
 
 
 
--- spec/directory_spec.rb
+++ spec/directory_spec.rb
@@ -2,7 +2,38 @@ require 'directory'
 
 describe 'student directory' do
 
-	
+	before(:each) do
+		@writer = double('writer')
+		@reader = double('reader')
+    @dir = StudentDirectory.new(@writer, @reader)
+  	end
+ 
+	context 'loading data' do
+
+		it 'there are no students' do
+			expect(@dir.students).to be_empty
+		end
+
+		it 'loads from a file' do
+			
+			expect(CSV).to receive(:foreach).with('students.csv')
+			@dir.load_students_from('students.csv')
+		end
+
+		it 'imports a student' do
+			
+			student = {name:'Nic',cohort:'may',year:2014}
+			@dir.import(student)
+			expect(@dir.students).to eq [student]
+		end
+
+		it 'creates a student' do
+			
+			student = {name:'Nic',cohort:'may',year:2014}
+		  expect(@dir.import_from(['Nic','may',2014])).to eq student
+		end
+
+	end
 
 
 end
