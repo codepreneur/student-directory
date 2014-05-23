10720d5288bd85115e3e6fdb9bf761d6857d6766
gomenasaai <contramundee@gmail.com>
2014-05-23 14:46:10 +0100

    added functionality: user interactivity and final unit tests

--- lib/directory.rb
+++ lib/directory.rb
@@ -47,5 +47,109 @@ class StudentDirectory
 	end
 
 
+	def interact_with_menu
+		option = @reader.call.chomp
+		case option
+		when "1" then gather_students
+		when "2" then list_students(students)
+		when "3" then save_students_to('students.csv',students)
+		when "4" then load_students_from('students.csv')
+		when "5" then delete
+		when "6" then edit	
+		when "9" then exit
+		# when "9" then @exit = true
+		else print_menu
+		end
 
-end
+	end
+
+
+	def print_menu
+		menu  = "1. Input students\n"
+		menu += "2. List/display students\n"
+		menu += "3. Save changes to file\n"
+		menu += "4. Load from file to array\n"
+		menu += "5. Delete a student\n"
+		menu += "6. Edit a student\n"
+		menu += "9. Exit\n"
+	end
+
+
+	def control_flow
+		load_students_from('students.csv')
+		@writer.call "Welcome to student directory!"
+		
+		while not @exit == true
+			@writer.call print_menu
+			interact_with_menu
+		end
+	end
+
+	def delete
+		_name = prompt_name
+		students.delete_if{|student| student[:name] == _name}
+	end
+
+
+	def edit
+		student_to_edit  = prompt_name
+		detail_to_change = change_what?
+		detail_changed   = to_what?
+		change(student_to_edit, detail_to_change, detail_changed)
+	end
+
+
+	def gather_students
+  	_name = prompt_name
+		_cohort = prompt_cohort
+		_year = prompt_year
+		student = {name: _name, cohort: _cohort, year: _year}
+		import(student)
+	end
+
+
+	def prompt_name
+		@writer.call "Name?"
+		@reader.call.chomp
+	end
+
+
+	def prompt_cohort
+		@writer.call "Cohort?"
+		@reader.call.chomp
+	end
+
+
+	def prompt_year
+		@writer.call "Year?"
+		@reader.call.chomp
+	end
+
+
+
+	def change_what?
+		@writer.call "Enter name to change name, cohort to change cohort or year to change year."
+		key = @reader.call.chomp.to_sym
+		raise 'Not a chance' unless [:name, :cohort, :year].include? key
+		key
+	end
+
+	def to_what?
+		@writer.call "to what?"
+		@reader.call.chomp
+	end
+
+	def change(student_details, detail_to_change, detail_changed)
+		students.each do |student|
+			student[detail_to_change.to_sym] = detail_changed if student[:name] == student_details
+		end
+	end
+
+
+	end
+
+	if __FILE__ == $0
+		StudentDirectory.new(method(:puts), method(:gets)).control_flow
+	end
+
+
--- spec/directory_spec.rb
+++ spec/directory_spec.rb
@@ -66,7 +66,91 @@ describe 'student directory' do
 
 	end
 
-	
+	context 'takes user input and reacts when' do
 
+		def foo
+			'This will be overridden'
+		end
+
+		it 'proof that stub! works' do
+			allow(self).to receive(:foo).and_return('something else')
+		end
+
+		it '1 to take inputs and shovel them into students' do
+
+
+			allow(@dir).to receive(:prompt_name).and_return('Nic')
+			allow(@dir).to receive(:prompt_cohort).and_return('may')
+			allow(@dir).to receive(:prompt_year).and_return(2014)
+
+			allow(@reader).to receive(:call).and_return('1')
+			@dir.interact_with_menu()
+
+			expect(@dir.students).to eq [{name:'Nic',cohort:'may',year:2014}]
+			
+			
+		end 
+
+
+
+		it '2 to list inputed students' do
+			
+			@dir.import({name:'Nic',cohort:'may',year:2014})
+
+			allow(@reader).to receive(:call).and_return('2')
+			allow(@writer).to receive(:call).and_return("Nic from may cohort in 2014")
+
+			expect(@dir.interact_with_menu()).to eq "Nic from may cohort in 2014"
+		end
+
+
+		it '3 to save inputed students to file' do
+			
+			@dir.import({name:'Nic',cohort:'may',year:2014})
+			allow(@reader).to receive(:call).and_return('3')
+			expect(@dir.interact_with_menu()).to eq [{name:'Nic',cohort:'may',year:2014}]
+		end
+
+		it '4 to list students from a file' do
+			
+			students = @dir.import({name:'Nic',cohort:'may',year:2014})
+			@dir.save_students_to('students.csv',@dir.students)
+			allow(@reader).to receive(:call).and_return('4')
+			@dir.interact_with_menu()
+			expect(@dir.students.include?(students.first)).to be_true
+		end
+
+		it '5 to delete a student' do
+			@dir.import({name:'Nic',cohort:'may',year:2014})
+
+
+			allow(@dir).to receive(:prompt_name).and_return('Nic')
+		
+			allow(@reader).to receive(:call).and_return('5')
+			@dir.interact_with_menu()
+
+			expect(@dir.students).to eq []
+		end
+
+
+		it '6 to edit a student' do
+			@dir.import({name: 'Sue', cohort: 'june', year: 2014})
+
+			allow(@reader).to receive(:call)
+				.and_return('Sue')
+				.and_return('cohort')
+				.and_return('may')
+
+			@dir.change('Sue', 'cohort', 'may')
+			expect(@dir.students.first).to eq({name: 'Sue', cohort: 'may', year: 2014})
+		end
+
+
+		it '9 to exit' do
+			allow(@reader).to receive(:call).and_return('9')
+			expect{@dir.interact_with_menu()}.to raise_error SystemExit
+		end
+		
+	end
 
 end
